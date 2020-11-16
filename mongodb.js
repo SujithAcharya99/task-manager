88a1fe93b594a8b4eb5f2dbadcb8aca72cf24764
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-16 19:45:46 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -11,4 +11,114 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, clent) => {
        return console.log('connection error...!');
     }
         console.log('connected successfully...!')
+  
+   //     ****************** INSERT ONE ******************* */
+
+        db.collection('users').insertOne({
+            name : 'Sharath',
+            age : 22
+        }, (error, result) => {
+            if(error){
+                return console.log('unable to insert data');
+            }
+
+            console.log(result.ops);
+
+        });
+//**************************** INSERT MANY ************************ */
+
+        db.collection('users').insertMany([
+            {
+                name : 'Sanjay',
+                age : 22
+            },{
+                name : 'thanush',
+                age : 22
+            }
+        ], (error , result) => {
+
+            if (error) {
+               return console.log('unable to insert data');
+            }
+            console.log(result.ops);
+        } );
+
+        db.collection('tasks').insertMany([
+            {
+                description : 'note-app',
+                completed : true
+            },{
+                description : 'weather-app',
+                completed : true
+            },{
+                description : 'task-app',
+                completed : false
+            }
+        ], (error , result) => {
+
+            if (error) {
+                return console.log('unable to insert data');
+            }
+            console.log(result.ops);
+        } ); 
+
+
+    //    ***************** FIND ONE ************************* */
+
+        db.collection('users').findOne({_id: new ObjectID("5fb1fb16e5236e15209a00f9")}, (error, user) => {
+            if (error) {
+                return console.log('unable to fetch data');
+                
+            }
+
+            console.log(user);
+        });
+
+//************************* FIND MANY ******************************* */
+
+        db.collection('users').find({age : 22}).toArray((error, user) => {
+            console.log(user);
+        });
+         
+
+//        ********************* UPDATE ONE ************************** */
+
+        db.collection('users').updateOne({
+            _id: new ObjectID("5fb1f3f4a747921253ccdee2")
+        },{
+            $set : {
+                name : 'Mike'
+            }
+        }).then((result) => {
+            console.log(result);
+        }).catch(() => {
+            console.log(error);
+        });
+
+
+        //************************ UPDATE MANY ******************** */
+
+        db.collection('tasks').updateMany({
+            completed: false
+        },{
+            $set : {
+                completed : true
+            }
+        }).then((result) => {
+            console.log(result.matchedCount);
+        }).catch((error) => {
+            console.log(error);
+        });
+
+
+       // ********************************** DELETE MANY **************************** */
+
+        db.collection('users').deleteMany({
+            age: 25
+        }).then((result) => {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        });
+    
 });
